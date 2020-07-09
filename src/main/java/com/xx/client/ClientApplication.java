0505f3d4afe92aaf6aece34d97ebe358fe8045dd
thisBreak <zo10010@163.com>
2020-07-09 14:18:03 +0800

    第一次

---
+++ src/main/java/com/xx/client/ClientApplication.java
@@ -0,0 +1,24 @@
+package com.xx.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ClientApplication {
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
